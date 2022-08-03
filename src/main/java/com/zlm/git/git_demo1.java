cf59781a9642e9a589378f2f1a3d0c7e8ec84020
Lemon <Lesom@zlm.com>
2022-08-03 17:28:22 +0800

    second commit

--- src/main/java/com/zlm/git/git_demo1.java
+++ src/main/java/com/zlm/git/git_demo1.java
@@ -3,5 +3,6 @@ package com.zlm.git;
 public class git_demo1 {
     public static void main(String[] args) {
         System.out.println("hello,get!");
+        System.out.println("hello,get2!");
     }
 }
